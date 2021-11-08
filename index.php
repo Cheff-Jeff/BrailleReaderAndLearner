2f6e1e80fdc2ea4cfd644854ad3ab50384f10c36
Jeffrey <1999jeffreynijkamp@gmail.com>
2021-11-08 09:33:42 +0100

    add routing

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  $request = $_SERVER['REQUEST_URI'];
+  switch ($request){
+    case '/':
+      require 'home.html';
+      die();
+    break;
+    case '/home':
+      require 'home.html';
+      die();
+    break;
+    case '/about':
+      require 'about.html';
+      die();
+    break;
+    default:
+      http_response_code(404);
+      require '404.html';
+      die();
+    break;
+  }
+?>
