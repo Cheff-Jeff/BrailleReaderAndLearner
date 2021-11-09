6dcd7580adaa4bd95e18361e145999a1f6c34d12
Jeffrey <1999jeffreynijkamp@gmail.com>
2021-11-09 10:34:17 +0100

    update routing

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
   $request = $_SERVER['REQUEST_URI'];
   switch ($request){
     case '/':
-      require 'home.html';
+      require './home.html';
       die();
     break;
     case '/home':
-      require 'home.html';
+      require './home.html';
       die();
     case '/index.php':
-      require 'home.html';
+      require './home.html';
       die();
     break;
     case '/about':
-      require 'about.html';
+      require './about.html';
       die();
     break;
     default:
